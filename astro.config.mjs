76f7fe51f928484532942f9e465c9b33a8f912da
unknown <javiervitalioperalta@gmail.com>
2024-10-01 15:05:04 -0700

    Astro + Sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import sanity from '@sanity/astro'
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 
-// https://astro.build/config
-export default defineConfig({});
+import tailwind from '@astrojs/tailwind';
+
+export default defineConfig({
+  integrations: [sanity({
+    projectId: '14be0l9l',
+    dataset: 'production',
+    // Set useCdn to false if you're building statically.
+    useCdn: false,
+    apiVersion: "2024-09-21", // insert the current date to access the latest version of the API
+    studioBasePath: '/admin',
+    stega: {
+      studioUrl: "/admin",
+    },
+  }), react(), tailwind()],
+})
